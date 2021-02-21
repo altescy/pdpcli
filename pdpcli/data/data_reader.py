3d85c2483f88d857cd962e631d6fed034c0ff60b
altescy <altescy@fastmail.com>
2021-02-22 03:46:17 +0900

    use sqlalchemy to read database

--- pdpcli/data/data_reader.py
+++ pdpcli/data/data_reader.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from pathlib import Path
 from urllib.parse import urlparse
-import importlib
+from sqlalchemy import create_engine
 
 import colt
 import pandas
@@ -53,32 +53,6 @@ class SqlDataReader(DataReader):
     def read(self, file_path: Path) -> pandas.DataFrame:
         with open(file_path) as fp:
             query = fp.read()
-        conn = self._get_db_connection()
-        return pandas.read_sql(query, conn, **self._kwargs)
-
-    def _get_db_connection(self):
-        parsed_dsn = urlparse(self._dsn)
-        scheme = parsed_dsn.scheme
-        path = parsed_dsn.path
-        if scheme == "mysql":
-            return self._get_mysql_connection(self._dsn)
-        if scheme == "postgres":
-            return self._get_pgsql_connection(self._dsn)
-        if scheme == "sqlite3":
-            return self._get_sqlite_connection(path)
-        raise ConfigurationError(f"Invalid database scheme: {scheme}")
-
-    @staticmethod
-    def _get_pgsql_connection(dsn: str):
-        psycopg2 = importlib.import_module("psycopg2")
-        return psycopg2.connect(dsn)  # type: ignore
-
-    @staticmethod
-    def _get_mysql_connection(dsn: str):
-        MySQLdb = importlib.import_module("MySQLdb")
-        return MySQLdb.connect(dsn)  # type: ignore
-
-    @staticmethod
-    def _get_sqlite_connection(path: str):
-        import sqlite3  # pylint: disable=import-outside-toplevel
-        return sqlite3.connect(path)
+        engine = create_engine(self._dsn)
+        with engine.connect() as connection:
+            return pandas.read_sql(query, connection, **self._kwargs)
