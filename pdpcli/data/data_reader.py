33eb0347ae4e50f436bd06a4ce57aa5925d1381f
altescy <altescy@fastmail.com>
2021-02-22 02:30:19 +0900

    add sql reader

--- pdpcli/data/data_reader.py
+++ pdpcli/data/data_reader.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 from pathlib import Path
+from urllib.parse import urlparse
 
 import colt
 import pandas
+
+from pdpcli.exceptions import ConfigurationError
 
 
 class DataReader(colt.Registrable):
@@ -38,3 +41,43 @@ class PickleDataReader(DataReader):
 
     def read(self, file_path: Path) -> pandas.DataFrame:
         return pandas.read_pickle(file_path, *self._args, **self._kwargs)
+
+
+@DataReader.register("sql")
+class SqlDataReader(DataReader):
+    def __init__(self, dsn, **kwargs) -> None:
+        self._dsn = dsn
+        self._kwargs = kwargs
+
+    def read(self, file_path: Path) -> pandas.DataFrame:
+        with open(file_path) as fp:
+            query = fp.read()
+        conn = self._get_db_connection()
+        return pandas.read_sql(query, conn, **self._kwargs)
+
+    def _get_db_connection(self):
+        parsed_dsn = urlparse(self._dsn)
+        scheme = parsed_dsn.scheme
+        path = parsed_dsn.path
+        if scheme == "mysql":
+            return self._get_mysql_connection(self._dsn)
+        if scheme == "postgres":
+            return self._get_pgsql_connection(self._dsn)
+        if scheme == "sqlite3":
+            return self._get_sqlite_connection(path)
+        raise ConfigurationError(f"Invalid database scheme: {scheme}")
+
+    @staticmethod
+    def _get_pgsql_connection(dsn: str):
+        import psycopg2  # pylint: disable=import-outside-toplevel
+        return psycopg2.connect(dsn)
+
+    @staticmethod
+    def _get_mysql_connection(dsn: str):
+        import MySQLdb  # pylint: disable=import-outside-toplevel
+        return MySQLdb.connect(dsn)
+
+    @staticmethod
+    def _get_sqlite_connection(path: str):
+        import sqlite3  # pylint: disable=import-outside-toplevel
+        return sqlite3.connect(path)
