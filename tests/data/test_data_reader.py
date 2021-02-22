4ff830b00dce2ddc944c2b0640e3695236c91d26
altescy <altescy@fastmail.com>
2021-02-22 22:15:56 +0900

    add tests for data readers

---
+++ tests/data/test_data_reader.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import pandas
+
+from pdpcli.data.data_reader import (
+    CsvDataReader,
+    TsvDataReader,
+    JsonLinesDataReader,
+    PickleDataReader,
+    SqlDataReader,
+)
+
+FIXTURE_DIR = Path("./tests/fixture/")
+
+
+def test_csv_data_reader():
+    input_file = FIXTURE_DIR / "data" / "data.csv"
+    reader = CsvDataReader()
+    df = reader.read(input_file)
+
+    assert isinstance(df, pandas.DataFrame)
+
+
+def test_tsv_data_reader():
+    input_file = FIXTURE_DIR / "data" / "data.tsv"
+    reader = TsvDataReader()
+    df = reader.read(input_file)
+
+    assert isinstance(df, pandas.DataFrame)
+
+
+def test_jsonl_data_reader():
+    input_file = FIXTURE_DIR / "data" / "data.jsonl"
+    reader = JsonLinesDataReader()
+    df = reader.read(input_file)
+
+    assert isinstance(df, pandas.DataFrame)
+
+
+def test_pickle_data_reader():
+    input_file = FIXTURE_DIR / "data" / "data.pkl"
+    reader = PickleDataReader()
+    df = reader.read(input_file)
+
+    assert isinstance(df, pandas.DataFrame)
+
+
+def test_sql_data_reader():
+    input_file = FIXTURE_DIR / "data" / "query.sql"
+    dsn = "sqlite:///tests/fixture/data/data.db"
+    reader = SqlDataReader(dsn=dsn)
+    df = reader.read(input_file)
+
+    assert isinstance(df, pandas.DataFrame)
