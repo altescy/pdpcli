8db67031583a1f4e4db6a2fa3276890e5d78ee25
altescy <altescy@fastmail.com>
2021-02-22 22:04:29 +0900

    add tests for commands

---
+++ tests/commands/test_apply.py
@@ -0,0 +1,98 @@
+import tempfile
+from pathlib import Path
+
+import pandas
+
+from pdpcli.commands import create_parser
+from pdpcli.commands.apply import ApplyCommand
+
+
+def test_apply_from_config():
+    fixture_path = Path("tests/fixture")
+    config_path = fixture_path / "configs" / "config.yml"
+    input_file = fixture_path / "data" / "data.csv"
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        output_file = tempdir / "output.csv"
+
+        parser = create_parser()
+        args = parser.parse_args([
+            "apply",
+            str(config_path),
+            str(input_file),
+            "-o",
+            str(output_file),
+        ])
+
+        args.func(args)
+
+        assert output_file.is_file()
+
+        input_df = pandas.read_csv(input_file)
+        output_df = pandas.read_csv(output_file)
+
+        assert len(input_df) == len(output_df)
+        assert "name" not in output_df.columns
+        assert "job" not in output_df.columns
+
+
+def test_apply_from_pickled_model():
+    fixture_path = Path("tests/fixture")
+    pipeline_path = fixture_path / "data" / "pipeline.pkl"
+    input_file = fixture_path / "data" / "data.csv"
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        output_file = tempdir / "output.csv"
+
+        parser = create_parser()
+        args = parser.parse_args([
+            "apply",
+            str(pipeline_path),
+            str(input_file),
+            "-o",
+            str(output_file),
+        ])
+
+        args.func(args)
+
+        assert output_file.is_file()
+
+        input_df = pandas.read_csv(input_file)
+        output_df = pandas.read_csv(output_file)
+
+        assert len(input_df) == len(output_df)
+        assert "name" not in output_df.columns
+        assert "job" not in output_df.columns
+
+
+def test_apply_with_cli_args():
+    fixture_path = Path("tests/fixture")
+    config_path = fixture_path / "configs" / "config.yml"
+    input_file = fixture_path / "data" / "data.csv"
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        output_file = tempdir / "output.csv"
+
+        parser = create_parser()
+        args = parser.parse_args([
+            "apply",
+            str(config_path),
+            str(input_file),
+            "pipeline.stages.drop_columns.columns=[name]",
+            "-o",
+            str(output_file),
+        ])
+
+        args.func(args)
+
+        assert output_file.is_file()
+
+        input_df = pandas.read_csv(input_file)
+        output_df = pandas.read_csv(output_file)
+
+        assert len(input_df) == len(output_df)
+        assert "name" not in output_df.columns
+        assert "job" in output_df.columns
---
+++ tests/commands/test_build.py
@@ -0,0 +1,48 @@
+import tempfile
+from pathlib import Path
+
+from pdpcli.commands import create_parser
+from pdpcli.commands.build import BuildCommand
+
+
+def test_build_without_input_file():
+    fixture_path = Path("tests/fixture")
+    config_path = fixture_path / "configs" / "config.yml"
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        pipeline_path = tempdir / "pipeline.pkl"
+
+        parser = create_parser()
+        args = parser.parse_args([
+            "build",
+            str(config_path),
+            str(pipeline_path),
+        ])
+
+        args.func(args)
+
+        assert pipeline_path.is_file()
+
+
+def test_build_with_input_file():
+    fixture_path = Path("tests/fixture")
+    config_path = fixture_path / "configs" / "config.yml"
+    input_file = fixture_path / "data" / "data.csv"
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = Path(tempdir)
+        pipeline_path = tempdir / "pipeline.pkl"
+
+        parser = create_parser()
+        args = parser.parse_args([
+            "build",
+            str(config_path),
+            str(pipeline_path),
+            "-i",
+            str(input_file),
+        ])
+
+        args.func(args)
+
+        assert pipeline_path.is_file()
