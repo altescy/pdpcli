b90e365d424066c523a5d6d59b0758868ccfe239
altescy <altescy@fastmail.com>
2021-02-22 20:44:50 +0900

    load pdpie stages explicitly

--- pdpcli/stages/__init__.py
+++ pdpcli/stages/__init__.py
@@ -1,3 +1,7 @@
+from pdpcli.stages.pdpipe import load_pdpie_stages
+load_pdpie_stages()
+
+#pylint: disable=wrong-import-position
 from pdpcli.stages.pipeline import Pipeline
 from pdpcli.stages.pass_through_stage import PassThroughStage
 from pdpcli.stages.sklearn import TfidfVectorizeTokenLists
