91ff7f65d967692637e1c050d787b95074b351af
altescy <altescy@fastmail.com>
2021-02-24 18:05:13 +0900

    add test for config builder

---
+++ tests/configs/test_config_builder.py
@@ -0,0 +1,25 @@
+from pdpcli.configs.config_builder import ConfigBuilder
+from pdpcli.data import DataReader, DataWriter
+from pdpcli.stages import Stage
+
+
+def test_config_builder():
+    config = {
+        "colt": {
+            "typekey": "!"
+        },
+        "reader": {
+            "!": "csv",
+        },
+        "writer": {
+            "!": "jsonl",
+        },
+        "pipeline": {
+            "!": "pass_through",
+        },
+    }
+
+    builder = ConfigBuilder.build(config)
+    assert isinstance(builder.reader, DataReader)
+    assert isinstance(builder.writer, DataWriter)
+    assert isinstance(builder.pipeline, Stage)
