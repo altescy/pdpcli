87a92638f9d24254e706288a07cc45821548a443
altescy <altescy@fastmail.com>
2021-06-26 01:13:39 +0900

    separate SelectColumns & RexColumns

--- pdpcli/stages/select_columns.py
+++ pdpcli/stages/select_columns.py
@@ -13,6 +13,22 @@ class SelectColumns(Stage):
             columns = [columns]
 
         super().__init__(**kwargs)
+        self._columns = columns
+
+    def _prec(self, df: pandas.DataFrame) -> bool:
+        return True
+
+    def _transform(self, df: pandas.DataFrame, verbose: bool) -> pandas.DataFrame:
+        return df[self._columns]
+
+
+@Stage.register("rex_columns")
+class RexColumns(Stage):
+    def __init__(self, columns: Union[str, List[str]], **kwargs: Any) -> None:
+        if isinstance(columns, str):
+            columns = [columns]
+
+        super().__init__(**kwargs)
         self._columns = [re.compile(c) for c in columns]
 
     def _prec(self, df: pandas.DataFrame) -> bool:
--- tests/stages/test_select_columns.py
+++ tests/stages/test_select_columns.py
@@ -1,15 +1,27 @@
 import pandas
 
-from pdpcli.stages.select_columns import SelectColumns
+from pdpcli.stages.select_columns import RexColumns, SelectColumns
 
 
 def test_select_columns() -> None:
+    df = pandas.DataFrame()
+    df["foo"] = ["foo", "bar", "baz"]
+    df["bar"] = [10, 20, 30]
+    df["baz"] = ["a", "b", "c"]
+
+    stage = SelectColumns(["foo", "baz"])
+    output = stage.apply(df)
+
+    assert list(output.columns) == ["foo", "baz"]
+
+
+def test_rex_columns() -> None:
     df = pandas.DataFrame()
     df["col_1"] = ["foo", "bar", "baz"]
     df["col_2"] = [10, 20, 30]
     df["xyz"] = ["a", "b", "c"]
 
-    stage = SelectColumns(r"col_\d+")
+    stage = RexColumns(r"col_\d+")
     output = stage.apply(df)
 
     assert sorted(output.columns) == ["col_1", "col_2"]
